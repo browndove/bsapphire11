e32f8e80f98b8d1d269cb3f0b587d4b65927c3b0
bluebird <bluebird23szn@gmail.com>
2024-08-06 11:30:16 +0000

    changes

---
+++ app/(site)/page.tsx
@@ -0,0 +1,38 @@
+import { Metadata } from "next";
+import Hero from "@/components/Hero";
+import Brands from "@/components/Brands";
+import Feature from "@/components/Features";
+import About from "@/components/About";
+import FeaturesTab from "@/components/FeaturesTab";
+
+import Integration from "@/components/Integration";
+
+import Contact from "@/components/Contact";
+import Blog from "@/components/Blog";
+import Testimonial from "@/components/Testimonial";
+
+export const metadata: Metadata = {
+  title: "Black Shapphire",
+  description: "Official website of Blvcksapphire Ltd.",
+  // other metadata
+};
+
+export default function Home() {
+  return (
+    <main>
+      <Hero />
+      <Brands />
+      <Feature />
+      <About />
+      <FeaturesTab />
+    
+      <Integration />
+     
+      
+      <Testimonial />
+      
+      <Contact />
+    
+    </main>
+  );
+}
---
+++ components/FeaturesTab/featuresTabData.tsx
@@ -0,0 +1,33 @@
+import { FeatureTab } from "@/types/featureTab";
+
+const featuresTabData: FeatureTab[] = [
+  {
+    id: "tabOne",
+    title: " Driving innovation and excellence in IT solutions.",
+    desc1: `We’re committed to creating positive change in Africa and ensuring we act as responsible business leaders by adopting 
+    a shared blueprint to help achieve SDG goals 8, 9, and 11`,
+    desc2: `   
+Together, we innovate for a sustainable future in Africa.`,
+    image: "/replace.jpg",
+    imageDark: "/replace.jpg",
+  },
+  {
+    id: "tabTwo",
+    title: "Leveraging Responsible AI for sustainable and ethical solutions.",
+    desc1: `Powerful AI tools like generative AI bring unprecedented opportunities as well as new risks. We help clients to take intentional actions to design, deploy, and use AI to create value and build trust.`,
+    desc2: `   
+Ensuring AI-driven growth while safeguarding trust and integrity.`,
+    image: "/replace.jpg",
+    imageDark: "/replace.jpg",
+  },
+  {
+    id: "tabThree",
+    title: "Championing Inclusion & Diversity to foster innovation and growth",
+    desc1: `We hire and develop people who have different backgrounds, different perspectives, and different lived experiences.`,
+    desc2: `These differences ensure that we have and attract the cognitive diversity to deliver a variety of perspectives, observations, and insights which are essential to drive the innovation needed to grow.`,
+    image: "/replace.jpg",
+    imageDark: "/replace.jpg",
+  },
+];
+
+export default featuresTabData;
