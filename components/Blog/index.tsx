9222d1cec3ad651cccc9298c16882a80b3ba2c19
bluebird <bluebird23szn@gmail.com>
2024-08-07 14:46:26 +0000

    image fixed

--- components/Blog/index.tsx
+++ components/Blog/index.tsx
@@ -39,8 +39,8 @@ const Blog = async () => {
       <div className="mx-auto mt-15 max-w-c-1280 px-4 md:px-8 xl:mt-20 xl:px-0">
         <div className="grid grid-cols-1 gap-7.5 md:grid-cols-2 lg:grid-cols-3 xl:gap-10">
           {blogs.map((blog, index) => (
-            <div key={index} className="border rounded-lg overflow-hidden shadow-lg bg-white dark:bg-gray-800">
-              <img src={blog.image} alt={blog.title} className="w-full h-48 object-cover" />
+            <div key={index} className=" rounded-lg overflow-hidden shadow-lg  dark:bg-gray-800">
+              <img src={blog.image} alt={blog.title} className="object-cover object-center w-[100%] " />
               <div className="p-4">
                 <h2 className="text-xl font-bold mb-2 text-gray-900 dark:text-gray-100">{blog.title}</h2>
                 <p className="text-gray-700 dark:text-gray-300">{blog.description}</p>
