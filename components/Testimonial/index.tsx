bab34adcd81e5c88e735138cf95cb7099283b06a
chris <bluebird23szn@gmail.com/>
2024-09-17 18:28:02 -0700

    logo

--- components/Testimonial/index.tsx
+++ components/Testimonial/index.tsx
@@ -21,7 +21,7 @@ const Testimonial = () => {
             <SectionHeader
               headerInfo={{
                 title: `Job Archives`,
-                subtitle: `Join Our Team at Blvck Sapphire`,
+                subtitle: `Join our team at BLVCK SAPPHIRE`,
                 description: ` We're on a mission to redefine the future of technology and cybersecurity. We are excited to offer a range of career opportunities that allow you to be part of our innovative team. Explore the current openings and find out how you can contribute to groundbreaking projects in AI, ML, and cybersecurity while growing your career with us.`,
               }}
             />
